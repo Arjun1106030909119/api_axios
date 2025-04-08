e9791a9f2dd18633662c274eb895e9dbd6b79d20
Arjun V <pallatharjun119@gmail.com>
2025-04-08 22:38:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const url="https://jsonplaceholder.typicode.com/"
+  return (
+    
+    </div>
+  );
+}
+
+export default App;
